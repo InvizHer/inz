5f333e18c134ccf2e8842c81e6834b422c9f3b8f
InvizHer <112791490+InvizHer@users.noreply.github.com>
2024-11-07 15:09:08 +0530

    Delete posts/js directory

--- posts/js/config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-    GITHUB_USERNAME: 'InvizHer',
-    GITHUB_REPO: 'https://github.com/InvizHer/Articles',
-    POSTS_PER_PAGE: 9,
-    GITHUB_API_BASE: 'https://api.github.com/repos',
-    RAW_CONTENT_BASE: 'https://raw.githubusercontent.com'
-};
